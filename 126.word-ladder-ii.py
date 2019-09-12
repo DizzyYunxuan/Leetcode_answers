0029f5c6e1c59bc948fe67ec1a9aabd38bb894b7
DizzyYunxuan <516488199@qq.com>
2019-09-12 19:59:59 +0800

    12th Sep No.126 is the most difficult problem that I ever meet....

--- 126.word-ladder-ii.py
+++ 126.word-ladder-ii.py
@@ -4,6 +4,39 @@
 # [126] Word Ladder II
 #
 class Solution:
-    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-        
+    def findLadders(self, beginWord: str, endWord: str, wordList):
+        from collections import defaultdict
+        if endWord not in wordList: return []
+        forward,backward,wordList,dic = {beginWord},{endWord},set(wordList),defaultdict(set)
+        flag,letters,length = True,set('qwertyuioplkjhgfdsazxcvbnm'),len(endWord)
+        while forward:
+            if len(forward) > len(backward):
+                forward,backward,flag = backward,forward,not flag
+            cur = set()
+            wordList -= forward
+            for word in forward:
+                for idx in range(length):
+                    x,y = word[:idx],word[idx+1:]
+                    for letter in letters:
+                        temp = x + letter + y
+                        if temp in wordList:
+                            cur.add(temp)
+                            if flag: dic[temp].add(word)
+                            else: dic[word].add(temp)  
+            if cur & backward:
+                res = [[endWord]]
+                while res[0][0] != beginWord:
+                    res = [[x]+y for y in res for x in dic[y[0]]]
+                return res
+            forward = cur
+        return []
+# '"qa"\n"sq"\n["si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"]
+# "red"\n"tax"\n["ted","tex","red","tax","tad","den","rex","pee"]
+# if __name__ == "__main__":
+#     beginWord = "hit"
+#     endWord = "cog"
+#     wordList = ["hot","dot","dog","lot","log","cog"]
+#     # wordList = ["si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"]
+#     res = Solution().findLadders(beginWord, endWord, wordList)
+#     print(res)
 
--- 127.word-ladder.py
+++ 127.word-ladder.py
@@ -7,32 +7,99 @@ class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList) -> int:
         if endWord not in wordList:
             return 0
-        self.res = float('inf')
-        self.solver(beginWord, endWord, wordList, 0)
-        return 0 if self.res == float('inf') else self.res
-    def solver(self, begin, end, wordlist, path):
-        if begin == end:
-            self.res = min(self.res, path+1)
-        stack = self.canchange(begin, wordlist)
-        for i in stack:
-            idx = wordlist.index(i)
-            self.solver(i, end, wordlist[:idx]+wordlist[idx+1:], path+1)
-
-    def canchange(self, word, wordlist):
-        canchangelist = []
-        for cword in wordlist:
+        self.wdict = {}
+        self.builddict(wordList)
+        self.visited = {}
+        res = self.solver(beginWord, endWord)
+        return 0 if not res else res
+    
+    def solver(self, begin, end):
+        self.visited[begin] = True
+        stack = [(begin, 1)]
+        while stack:
+            cur, level = stack.pop(0)
+            for i in range(len(begin)):
+                inter = cur[:i]+'*'+cur[i+1:]
+                if inter in self.wdict:
+                    for i in self.wdict[inter]:
+                        if i == end:
+                            return level+1 
+                        if i not in self.visited:
+                            stack.append((i, level+1))
+                            self.visited[i] = True
+                    self.wdict[inter] = []
+         
+    def builddict(self, wordlist):
+        for word in wordlist:
             for i in range(len(word)):
-                t = word[:i] + cword[i] + word[i+1:]
-                if t == cword and t != word:
-                    canchangelist.append(cword)
-        return canchangelist
+                if word[:i]+'*'+word[i+1:] not in self.wdict:
+                    self.wdict[word[:i]+'*'+word[i+1:]] = [word]
+                else:
+                    self.wdict[word[:i]+'*'+word[i+1:]].append(word)
+# ✔ Accepted
+#   ✔ 40/40 cases passed (148 ms)
+#   ✔ Your runtime beats 62.17 % of python3 submissions
+#   ✔ Your memory usage beats 6.9 % of python3 submissions (17.3 MB)
 
+
+# from collections import defaultdict
+# class Solution(object):
+#     def ladderLength(self, beginWord, endWord, wordList):
+#         """
+#         :type beginWord: str
+#         :type endWord: str
+#         :type wordList: List[str]
+#         :rtype: int
+#         """
+
+#         if endWord not in wordList or not endWord or not beginWord or not wordList:
+#             return 0
+
+#         # Since all words are of same length.
+#         L = len(beginWord)
+
+#         # Dictionary to hold combination of words that can be formed,
+#         # from any given word. By changing one letter at a time.
+#         all_combo_dict = defaultdict(list)
+#         for word in wordList:
+#             for i in range(L):
+#                 # Key is the generic word
+#                 # Value is a list of words which have the same intermediate generic word.
+#                 all_combo_dict[word[:i] + "*" + word[i+1:]].append(word)
+
+
+#         # Queue for BFS
+#         queue = [(beginWord, 1)]
+#         # Visited to make sure we don't repeat processing same word.
+#         visited = {beginWord: True}
+#         while queue:
+#             current_word, level = queue.pop(0)      
+#             for i in range(L):
+#                 # Intermediate words for current word
+#                 intermediate_word = current_word[:i] + "*" + current_word[i+1:]
+
+#                 # Next states are all the words which share the same intermediate state.
+#                 for word in all_combo_dict[intermediate_word]:
+#                     # If at any point if we find what we are looking for
+#                     # i.e. the end word - we can return with the answer.
+#                     if word == endWord:
+#                         return level + 1
+#                     # Otherwise, add it to the BFS Queue. Also mark it visited
+#                     if word not in visited:
+#                         visited[word] = True
+#                         queue.append((word, level + 1))
+#                 all_combo_dict[intermediate_word] = []
+#         return 0
+# ✔ Accepted
+#   ✔ 40/40 cases passed (132 ms)
+#   ✔ Your runtime beats 76.31 % of python3 submissions
+#   ✔ Your memory usage beats 6.9 % of python3 submissions (17.2 MB)
 # if __name__ == "__main__":
-#     # beginWord = "hit"
-#     beginWord = "hot"
-#     # endWord = "cog"
-#     endWord = "dog"
+#     beginWord = "hit"
+#     # beginWord = "hot"
+#     endWord = "cog"
+#     # endWord = "dog"
 #     wordlist = ["hot","dot","dog","lot","log","cog"]
-#     wordlist = ["hot","dog"]
+#     # wordlist = ["hot","dog"]
 #     res = Solution().ladderLength(beginWord, endWord, wordlist)
 #     print(res)
