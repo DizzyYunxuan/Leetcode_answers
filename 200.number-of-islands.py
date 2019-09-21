c8dfbfcde05dc1e5298a4ef6870bc44db5609aba
DizzyYunxuan <516488199@qq.com>
2019-09-21 22:54:56 +0800

    21st Sep

--- 200.number-of-islands.py
+++ 200.number-of-islands.py
@@ -12,11 +12,9 @@ class Solution:
                     self.solver(i,j,grid)
                     res += 1
         return res
-
     def solver(self, x, y, grid):
         if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]):
-            return
-        
+            return    
         if grid[x][y] == '1':
             grid[x][y] = 'I'
         else:
@@ -25,4 +23,7 @@ class Solution:
         self.solver(x+1, y, grid)
         self.solver(x, y-1, grid)
         self.solver(x, y+1, grid)
-
+✔ Accepted
+  ✔ 47/47 cases passed (168 ms)
+  ✔ Your runtime beats 41.87 % of python3 submissions
+  ✔ Your memory usage beats 9.4 % of python3 submissions (14.7 MB)
