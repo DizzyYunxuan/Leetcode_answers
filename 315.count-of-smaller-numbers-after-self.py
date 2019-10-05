31a967ee7d6a127d6f72c9047551245478b34aec
DizzyYunxuan <516488199@qq.com>
2019-10-05 22:40:33 +0800

    5th Oct

--- 315.count-of-smaller-numbers-after-self.py
+++ 315.count-of-smaller-numbers-after-self.py
@@ -7,6 +7,15 @@
 # @lc code=start
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        
+        res = []
+        for i in range(len(nums)):
+            cur = nums[i]
+            count = 0
+            for j in range(i+1, len(nums)):
+                if nums[j] < cur:
+                    count += 1
+            res.append(count)
+        return res
+
 # @lc code=end
 
