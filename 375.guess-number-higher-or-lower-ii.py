e64d9bfd02b2c99ef04676d2fccfc262b3816d2f
DizzyYunxuan <516488199@qq.com>
2019-12-24 21:27:47 +0800

    24th Dec

--- 375.guess-number-higher-or-lower-ii.py
+++ 375.guess-number-higher-or-lower-ii.py
@@ -7,6 +7,25 @@
 # @lc code=start
 class Solution:
     def getMoneyAmount(self, n: int) -> int:
+        dp = [[0] * (n+1) for i in range(n+1)] 
         
+        for window in range(2, n + 1):
+            for start in range(1, n - window + 2):
+                minres = float('inf')
+                for piv in range(start + (window - 1) // 2, start + window - 1):
+                    res = piv + max(dp[start][piv-1], dp[piv+1][start + window - 1])
+                    minres = min(res, minres)
+                dp[start][start + window - 1] = minres
+        
+        return dp[1][n]
+
+Accepted
+13/13 cases passed (412 ms)
+Your runtime beats 85.15 % of python3 submissions
+Your memory usage beats 100 % of python3 submissions (13.3 MB)
+# if __name__ == "__main__":
+
+#     res = Solution().getMoneyAmount(5)
+
 # @lc code=end
 
--- 376.wiggle-subsequence.py
+++ 376.wiggle-subsequence.py
@@ -6,7 +6,36 @@
 
 # @lc code=start
 class Solution:
-    def wiggleMaxLength(self, nums: List[int]) -> int:
-        
+    def wiggleMaxLength(self, nums) -> int:
+        if len(nums) < 1: return len(nums)
+        res = 0
+        for start in range(len(nums)):
+            sq = nums[start]
+            aOrd, l = True, 1
+            for j in range(start+1, len(nums)):
+                if l < 2 and nums[j] != sq:
+                    if nums[j] - sq < 0:
+                        aOrd = True
+                    elif nums[j] - sq > 0:
+                        aOrd = False
+                    sq = nums[j]
+                    l += 1
+                    continue
+
+                if nums[j] - sq > 0 and aOrd:
+                    sq = nums[j]
+                    aOrd = False
+                    l += 1
+                elif nums[j] - sq < 0 and not aOrd:
+                    sq = nums[j]
+                    aOrd = True
+                    l += 1
+            res = max(res, l)
+        return res
+
+if __name__ == "__main__":
+    res = Solution().wiggleMaxLength([1,7,4,9,2,5])   
+            
+
 # @lc code=end
 
