e52bafdbec9a364f46ce2cc9a728000343e29c22
DizzyYunxuan <51648199@qq.com>
2020-07-13 00:38:01 +0800

    update-13th-July

---
+++ 386.lexicographical-numbers.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=386 lang=python3
+#
+# [386] Lexicographical Numbers
+#
+
+# @lc code=start
+# class Solution:
+#     def lexicalOrder(self, n: int) -> List[int]:
+#         a = range(1, n+1)
+#         a = list(map(lambda x:str(x), a))
+#         a.sort()
+#         return a
+# Accepted
+# 26/26 cases passed (124 ms)
+# Your runtime beats 56.41 % of python3 submissions
+# Your memory usage beats 97.97 % of python3 submissions (19.3 MB)
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        a = range(1, n+1)
+
+    def bin_sort(self, arr):
+        
+        
+
+# @lc code=end
+
