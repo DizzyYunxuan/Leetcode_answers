5a0580547c3821f0a64b03f21b43baf517badea2
DizzyYunxuan <51648199@qq.com>
2020-07-16 00:45:49 +0800

    update-16th-Jul

--- 386.lexicographical-numbers.py
+++ 386.lexicographical-numbers.py
@@ -7,22 +7,34 @@
 # @lc code=start
 # class Solution:
 #     def lexicalOrder(self, n: int) -> List[int]:
-#         a = range(1, n+1)
-#         a = list(map(lambda x:str(x), a))
-#         a.sort()
-#         return a
+#         return sorted(range(1, n+1), key=str)
 # Accepted
 # 26/26 cases passed (124 ms)
 # Your runtime beats 56.41 % of python3 submissions
 # Your memory usage beats 97.97 % of python3 submissions (19.3 MB)
 
 class Solution:
-    def lexicalOrder(self, n: int) -> List[int]:
-        a = range(1, n+1)
+    def lexicalOrder(self, n: int):
+        highDigit = 1
+        while highDigit * 10 <= n:
+            highDigit *= 10
+        higherDigit = highDigit * 10
+        withKeys = []
+        for i in range(1, n+1):
+            key = i
+            while key < highDigit:
+                key *= 10
+            withKeys.append(key * higherDigit + i)
+        withKeys.sort()
+        return [ki % higherDigit for ki in withKeys]
+ Accepted
+26/26 cases passed (116 ms)
+Your runtime beats 66.36 % of python3 submissions
+Your memory usage beats 58.79 % of python3 submissions (19.8 MB)       
+# if __name__ == "__main__":
+#     a = Solution().lexicalOrder(100)
+#     print(a)
 
-    def bin_sort(self, arr):
-        
-        
 
 # @lc code=end
 
---
+++ 388.longest-absolute-file-path.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode id=388 lang=python3
+#
+# [388] Longest Absolute File Path
+#
+
+# @lc code=start
+class Solution:
+    def lengthLongestPath(self, input: str) -> int:
+        if not input: return 0
+        print(input)
+        # tree = self.construct_tree(input)
+        # print(input.split(r'\n'))
+
+    def construct_tree(self, s):
+        tree, name, cur_path, i = {}, '', [], 0
+        while i < len(s):
+            if s[i] == 'n' and s[i-1] == '\\':
+                if '.' in name:
+                    
+                cur_path.append(name[:-1])
+                j, count = i
+                while s[j] != 'n' and s[j-1] != '\\':
+
+
+            
+            name += s[i]
+            i += 1
+                     
+                
+                
+            
+                
+
+
+
+
+
+# @lc code=end
+
